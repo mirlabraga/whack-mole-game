e7e2f44d066476779291e19f0ca82ac0d355600c
Mirla Chucre <mirlabraga@gmail.com>
2021-03-11 10:30:47 +0000

    remove css inline, add media screen and refactores functions js

--- src/js/whackGame.js
+++ src/js/whackGame.js
@@ -1,40 +1,49 @@
 let totalPoint = 0;
 let imagesShowed = [];
 let isPause = false;
+let cronometerTimer, runGameTimer = undefined;
+let timeoutGameInSec = timeoutGame / 1000;
 
 const addPoint = (event) => {
+
   event = event || window.event;
   const target = event.target || event.srcElement;
   const fileNameImage = target.src.toString();
 
   if (!fileNameImage.endsWith("hole.png")) {
-    totalPoint++;
-    document.getElementById("scores").innerText = totalPoint;
+    document.getElementById("scores").innerText = totalPoint++;
   }
 }
 
 const startGame = async () => {
-  const result = await runGame();
+  addMoles();
+  handleTimeoutGame();
+  finishGame();
 }
 
-const runGame = async () => {
-  setTimeout(() => {
-    setInterval(() => {
-      if (!isPause) {
-        randomMole();
+// Handle the timeout for the game
+const handleTimeoutGame = () => {
+  document.getElementById("timeout").innerText = timeoutGame / 1000;
+  cronometerTimer = setInterval(() => {
+    if (!isPause) {
+      timeoutGameInSec--;
+      document.getElementById("timeout").innerText = timeoutGameInSec;
+    }
+  }, 1000);
+}
+
+const addMoles = () => {
+  runGameTimer = setInterval(async () => {
+    if (!isPause) {
+      const isClean = await cleanMoles(imagesShowed);
+      if (isClean) {
+        addRandomMoles();
       }
-    }, timeoutMole);
-  }, timeoutGame)
+    }
+  }, timeoutMole);
 }
 
-const randomMole = async () => {
-  const isClean = await cleanMoles(imagesShowed);
-  if (isClean) {
-    addMoles();
-  }
-}
-
-const addMoles = async () => {
+const addRandomMoles = () => {
   let amountMoleWithal = Math.floor(Math.random() * maxSimultaneousMole) + 1;
   while (amountMoleWithal > 0) {
     const randomNumber = Math.floor(Math.random() * 9) + 1;
@@ -61,16 +70,22 @@ const cleanMoles = async () => {
 
 const stopGame = () => {
   isPause = !isPause;
-}
-
-const finishGame = () => {
-  alert(messages.finalGame);
+  clearInterval(cronometer);
 }
 
 const resetGame = () => {
   totalPoint = 0;
-  document.getElementById("scores").innerText = totalPoint;
+  document.getElementById("scores").innerText = 0;
+  timeoutGameInSec = timeoutGame / 1000;
   for (let index = 9; index > 0; index--) {
     document.getElementById(`img${index}`).src = `../images/hole.png`;;
   }
+}
+
+const finishGame = () => {
+  setTimeout(() => {
+    clearInterval(runGameTimer);
+    clearInterval(cronometerTimer);
+    alert(`Total Point:${totalPoint} \n${messages.finalGame}`)
+  }, timeoutGame);
 }
