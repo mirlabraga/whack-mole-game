6780b7452e2add934d9f3f8898ee82165f8c9549
Mirla Chucre <mirlabraga@gmail.com>
2021-03-11 10:29:17 +0000

    Refactores

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,9 @@
-const timeoutGame = 2000
+
+const timeoutGame = 10000
 const timeoutMole = 700
 const maxSimultaneousMole = 3
+
 const messages = {
   "help": `Whack Mole Game is a game where you win with the more points. All the time the mole show you need to click and you get a point. You have ${timeoutGame/1000} minutes for finish the game. You can stop and reset anytime.`,
-  "finalGame": "Congratuations!!! Let\'s play again?!"
+  "finalGame": "Congratuations!!! 🥳 \nLet\'s play again?! Click in Reset and Start consecutively."
 }
